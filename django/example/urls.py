12d438c95dd0e6be2e0e69dee6f9b89efc969c62
andrewcgaitskell <andrew@gaitskell.com>
2022-06-15 08:50:01 +0100

    added hello

--- django/example/urls.py
+++ django/example/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from django.urls import include, path
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('django_plotly_dash/', include('django_plotly_dash.urls')),
+    path('hello/', include('hello.urls')),
 ]
