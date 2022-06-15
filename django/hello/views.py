d97d19a44484def7eec7224baa8518e26e284fc9
andrewcgaitskell <andrew@gaitskell.com>
2022-06-15 13:07:50 +0100

    plotly chart works

--- django/hello/views.py
+++ django/hello/views.py
@@ -3,14 +3,41 @@ from django.views.generic import TemplateView # Import TemplateView
 # Create your views here.
 from django.http import HttpResponse
 
+#######
+from django.http import HttpResponse, HttpResponseRedirect
+from django.db import connections
+from django.contrib import messages
+from django.urls import reverse
+import pandas as pd
+from plotly.offline import plot
+import plotly.graph_objs as go
+from .plotly_plot import *
+
+#######
+
+
+#def index(request):
+#    return HttpResponse("Hello, world. You're at the polls index.")
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    
+    #Plotly visualizations
+    target_plot = plotly_plot()
+    
+    #Return context to home page view
+    context = {'target_plot': target_plot,}
+        
+    # Render the HTML template index.html with the data in the context variable.
+    return render(
+        request,
+        'index.html',
+        context= context)
+        
 
 
 # Add the two views we have been talking about  all this time :)
 class HomePageView(TemplateView):
-    template_name = "index.html"
+    template_name = "home.html"
 
 
 class AboutPageView(TemplateView):
